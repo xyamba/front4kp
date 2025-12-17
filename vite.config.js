36b44cbd69f015fee701ae4f37a377aa10004b07
xyamba <gagengagen06@gmail.com>
2025-12-17 14:16:01 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: './', 
+    base: '/front4kp/',
     server: {
         port: 3000,
         open: true
